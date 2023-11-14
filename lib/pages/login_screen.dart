6b84c004b818219bfad3865607939428c2e47466
vaibhav <vaibhavwaghre@gmail.com>
2023-11-14 23:57:57 +0530

    half login page design complete

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dart_fun_challange/utility/constants.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isFocused = false;
+  final TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     h = MediaQuery.of(context).size.height;
     w = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            buildDesign(),
-          ],
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              buildDesign(),
+              buildLoginText(),
+              buildFields(context),
+            ],
+          ),
         ),
       ),
     );
@@ -25,11 +37,53 @@ class LoginScreen extends StatelessWidget {
   Widget buildDesign() {
     return Container(
       color: Colors.red,
-      height: h! * 0.14,
-      child: SvgPicture.asset(
-        twitterIc,
+      height: h! * 0.18,
+      alignment: Alignment.topRight,
+      child: Image.asset(
+        bubbleImg,
         fit: BoxFit.cover,
       ),
+    );
+  }
+
+  Widget buildLoginText() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 20, right: 20),
+      color: Colors.amber,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Login',
+            style: GoogleFonts.roboto(
+              fontSize: 32,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Text(
+            'Please sign in to continue',
+            style: GoogleFonts.roboto(
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFields(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: 'email',
+          ),
+        )
+        // ),
+      ],
     );
   }
 }
--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -2,6 +2,6 @@ double? w;
 double? h;
 
 // Images
-const String bubbleImg = "assets/images/bubble_design.svg";
+const String bubbleImg = "assets/images/bubble_design.jpg";
 
 const String twitterIc = "assets/icons/twitter_ic.svg";
